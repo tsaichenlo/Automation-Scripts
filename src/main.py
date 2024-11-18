c2bf0cdc6d2ad3edf9df1d43e561aaef832483a6
Phoebe Lo <tsaichenlo33@gmail.com>
2024-11-18 09:50:41 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 from config import USERNAME, PASSWORD
+import sys
+import pyperclip
 
-browser = webdriver.Safari()
+
+# login process
+browser = webdriver.Chrome()
+
+browser.maximize_window()
 
 browser.get("https://github.com/login")
 
@@ -15,5 +21,40 @@ password_field.send_keys(PASSWORD)
 
 login_field.submit()
 
-time.sleep(5)  
-browser.quit()
+
+# create new repo
+try:
+    repo_name = sys.argv[1]
+except:
+    print('Please provide repo name.')
+    sys.exit()
+
+try:
+    visibility = sys.argv[2]
+except:
+    visibility = 'public'
+
+browser.get('https://github.com/new')
+
+name_field = browser.find_element(
+    By.XPATH, '//input[@data-testid="repository-name-input"]')
+name_field.send_keys(repo_name)
+
+if visibility == 'private':
+    visibility_field = browser.find_element(
+        By.XPATH, '//input[@name="visibilityGroup" and @value="private"]')
+else:
+    visibility_field = browser.find_element(
+        By.XPATH, '//input[@name="visibilityGroup" and @value="public"]')
+visibility_field.click()
+
+name_field.submit()
+
+try:
+    clipboard_button = browser.find_element(
+        By.XPATH, '//clipboard-copy[@for="empty-setup-push-repo-echo"]')
+    clipboard_button.click()
+
+    print(pyperclip.paste())
+except:
+    print(f'Repository creation failed. Repo name must be unique')
